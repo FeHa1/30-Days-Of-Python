28f1758f69a92e591375012635a1e8de3561b2a3
Federico Hayes <fedehayes2001@gmail.com>
2024-07-21 17:18:44 -0300

    Update variables.py

--- 00-pruebas/day_2/variables.py
+++ 00-pruebas/day_2/variables.py
@@ -99,6 +99,21 @@ The radius of a circle is 30 meters.
     Calculate the area of a circle and assign the value to a variable name of area_of_circle
     Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
     Take radius as user input and calculate the area.
+"""
+
+circle_radius = 30
+
+area_of_circle = 3.14 * 30**2
+print(area_of_circle)
+
+circum_of_circle = 2 * 3.14 * 30
+print(circum_of_circle)
+
+rad = float(input("¿Cual es el radio del circulo? "))
+area_of_circle_rad = 3.14 * rad**2
+print(area_of_circle_rad)
+
+"""
 Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
 Run help('keywords') in Python shell or in your file to check for the Python reserved words or keyword
 """
