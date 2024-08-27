d87f75d438da3260dad8a7986ba895cff975ae73
Federico Hayes <fedehayes2001@gmail.com>
2024-08-27 16:35:10 -0300

    Create exercise.py

---
+++ 00-pruebas/day_2/day_3/exercise.py
@@ -0,0 +1,123 @@
+"""
+Declare your age as integer variable
+Declare your height as a float variable
+Declare a variable that store a complex number
+Write a script that prompts the user to enter base and height of the triangle and calculate an area 
+of this triangle (area = 0.5 x b x h).
+"""
+
+"""
+age = int(22)
+print(age)
+
+height = float(1.73)
+print(height)
+
+complex_num = 1 + 1j
+print(complex_num)
+
+base_trinagle = float(input("¿Cual es la base del triangulo? "))
+height_triangle = float(input("¿Cual es la altura del triangulo? "))
+area = 0.5 * base_trinagle * height_triangle
+print("El área es: ", area)
+"""
+
+"""
+Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
+Calculate the perimeter of the triangle (perimeter = a + b + c).
+"""
+
+"""
+sideA = int(input("Ingrese el lado A del triangulo: "))
+sideB = int(input("Ingrese el lado B del triangulo: "))
+sideC = int(input("Ingrese el lado C del triangulo: "))
+
+perimeter = sideA + sideB + sideC
+print("El perímetro del triángulo es: ", perimeter)
+"""
+
+"""
+Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and 
+perimeter (perimeter = 2 x (length + width))
+Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference 
+(c = 2 x pi x r) where pi = 3.14.
+Calculate the slope, x-intercept and y-intercept of y = 2x - 2
+Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+Compare the slopes in tasks 8 and 9.
+Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value
+y is going to be 0.
+Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+Use and operator to check if 'on' is found in both 'python' and 'dragon'
+I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+There is no 'on' in both dragon and python
+Find the length of the text python and convert the value to float and convert it to string
+Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or 
+not using python?
+Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+Check if type of '10' is equal to type of 10
+Check if int('9.8') is equal to 10
+Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+"""
+
+"""
+lenght = float(input("¿Cual es el largo del trinagulo? "))
+width = float(input("¿Cual es el ancho del triangulo? "))
+print("El area del triangulo es: ", lenght*width)
+print("El perímetro del triangulo es: ", 2*(lenght+width))
+print("La circunferencia del circulo es: ", 2*3.14*)
+"""
+
+"""
+radio = float(input("¿Cual es el radio del ciruclo? "))
+print("El area de circulo es ", 3.14*radio**2)
+"""
+
+"""
+x1 = float(input("Ingrese el valor de X1: ")) #4
+y1 = float(input("Ingrese el valor de Y1: ")) #5
+x2 = float(input("Ingrese el valor de X2: ")) #8
+y2 = float(input("Ingrese el valor de Y2: ")) #7
+slope = (y2-y1) / (x2-x1)
+print("La pendiente es: ", slope)
+"""
+
+"""
+#x1=2
+#x2=6
+#y1=2
+#y2=10
+
+euclideanDistance = ((6-2) + (10-2))**0.5 #pongo 0.5 porque es la manera de hacer la raiz cuadrada
+print("La Distancia Eucladiana es: ", euclideanDistance)^
+"""
+
+"""
+x = float(input("Ingrese el valor de x: "))
+y = x**2 + 6*x + 9
+print("El valor de Y es: ", y) #para que Y sea 0 el valor de X tiene que ser -3
+"""
+
+"""
+print(len('python') != len('dragon'))
+"""
+
+"""
+print('on' in ('python' and 'dragon'))
+"""
+
+"""
+print('jargon' in 'I hope this course is not full of jargon')
+"""
+
+"""
+lenght = len('python')
+flo = float(lenght)
+string = str(flo)
+print(string)
+"""
+
+
+
+
+
+
